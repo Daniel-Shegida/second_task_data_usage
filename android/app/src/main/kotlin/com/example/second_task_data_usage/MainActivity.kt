3b40406fc0187d38c64afccabebde77363503855
daniel shegida <den.shegida@gmail.com>
2023-03-30 11:02:22 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/second_task_data_usage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.second_task_data_usage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/second_task/screen/tasks_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:second_task_data_usage/assets/strings/projects_strings.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text(ProjectStrings.title),
+      // ),
+      body: Container(color: Colors.white,),
+    );
+  }
+}
