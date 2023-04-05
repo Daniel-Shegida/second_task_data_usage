b5efe44234a9f0cdd46e488556f8f6fc10672503
daniel shegida <den.shegida@gmail.com>
2023-04-05 12:59:30 +0300

    refactor(info screen): polish info screen

---
+++ lib/features/info/screens/information_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:second_task_data_usage/assets/strings/projects_strings.dart';
+import 'package:second_task_data_usage/assets/text_styles/project_styles.dart';
+import 'package:second_task_data_usage/ui_kit/title_with_text_btn_widget.dart';
+import '../../../ui_kit/rounded_btn_widget.dart';
+
+class InformationScreen extends StatelessWidget {
+  const InformationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.only(left: 20.0.w, right: 14.w, bottom: 41.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const TextTitleWithBtn(
+                title: ProjectStrings.titleInfo,
+                isBtn: false,
+              ),
+              SingleChildScrollView(
+                child: Text(
+                  ProjectStrings.fishLong,
+                  style: ProjectStyles.semiBoldGrey15OpenSans,
+                ),
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              RoundedBtnWidget(bntText: ProjectStrings.btnInfo, onPressed: () {
+                Navigator.pop(context);
+              },),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
